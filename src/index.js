b38d11a5706eb4552e088850618a9f8dbc9be17b
Aleh Kapusta <alehkapusta@gmail.com>
2024-11-25 15:44:46 +0100

    change basename for github

--- src/index.js
+++ src/index.js
@@ -8,8 +8,7 @@ import reportWebVitals from './reportWebVitals';
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/sviatlana-kapusta">
-    
+    <BrowserRouter basename="/sviatlana-kapusta/build/">
       <App />
     </BrowserRouter>
   </React.StrictMode>

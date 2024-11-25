bbb54c46e6b2e84280cc92b7ea958d791db3307b
Aleh Kapusta <alehkapusta@gmail.com>
2024-11-25 15:58:44 +0100

    feat: vhange to hashrouter

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
 import AboutPage from './components/about_page/AboutPage';
 import MainPage from './components/main_page/MainPage';
@@ -12,8 +12,14 @@ function App() {
       <div className="container">
         <Routes>
           <Route path="sviatlana-kapusta/build/" element={<MainPage />} />
-          <Route path="sviatlana-kapusta/build/about-me" element={<AboutPage />} />
-          <Route path="sviatlana-kapusta/build/resume" element={<ResumePage />} />
+          <Route
+            path="sviatlana-kapusta/build/about-me"
+            element={<AboutPage />}
+          />
+          <Route
+            path="sviatlana-kapusta/build/resume"
+            element={<ResumePage />}
+          />
           <Route path="*" element={<p>ERROR: Page not found!</p>} />
         </Routes>
       </div>
